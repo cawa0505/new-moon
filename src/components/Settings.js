14a94050c580a0e130454059e3596bff5c8fdbb5
Steve Burtenshaw <steve.burtenshaw@gmail.com>
2017-06-09 17:34:33 +0100

    first electron commit

---
+++ src/components/Settings.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+import { FaCog } from 'react-icons/lib/fa'
+
+class Settings extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      url: ''
+    }
+  }
+
+  render() {
+    return (
+      <div className="settings">
+        <div className="container">
+          <div className="cog" onClick={this.props.onCog}><FaCog /></div>
+          <h2>Settings</h2>
+          <input placeholder="IP address" id="ip"/>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Settings
